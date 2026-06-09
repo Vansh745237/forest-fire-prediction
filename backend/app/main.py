89d457bf415cdb92848ba212b4698e8f12aa4246
Vansh745237 <vanshbathla125@gmail.com>
2026-06-10 03:33:39 +0530

    Fix CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5174",
-        "http://127.0.0.1:5174"
+        "http://127.0.0.1:5174",
+        "https://forest-fire-prediction-co9t.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
