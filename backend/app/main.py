b5e8660b68249d467579d2fbf2fa3df9c714617b
Vansh745237 <vanshbathla125@gmail.com>
2026-06-12 23:35:13 +0530

    Handle OPTIONS requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,18 +27,7 @@ print("CORS LOADED")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "http://localhost:5175",
-        "http://127.0.0.1:5175",
-        "https://forest-fire-prediction-zo7z.vercel.app",
-        "https://forest-fire-prediction-4yv1.vercel.app",
-        "https://forest-fire-prediction-taupe.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
